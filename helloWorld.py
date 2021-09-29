f46ecc32b57e365b8dede29c6c4b2a2bacae04a0
mingshin <ihateallofyou13@hotmail.com>
2021-09-29 10:47:18 +0800

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+import pandas as pd
+import numpy as np
+import os
+import sys
+import itertools
+import multiprocessing as mp
+import timeit
+import copy
+from tqdm import tqdm
+import re
+import subprocess
+from glob import glob
+import sklearn
+
+print("Successfully imported packages!")
+
+    
